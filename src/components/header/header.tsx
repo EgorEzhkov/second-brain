87e823ebaf2035a58d044f2bb2bd31443f994d0e
EgorEzhkov <egorkashpak21@yandex.ru>
2026-05-06 18:07:49 +0300

    start

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <div>Это будет мой header</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+import styles from './sidebar.module.css';
+
+const Sidebar = () => {
+  return <div className={styles.mainSidebar}>Пример</div>;
+};
+
+export default Sidebar;
